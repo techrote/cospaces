627269e09d97c59539e710bb0287972b42cf1d3c
tom <261801702+techrote@users.noreply.github.com>
2026-09-13 19:32:34 +0100

    feat: add GitHub CLI adapter seam

--- src/cospaces/adapters/github_cli.py
+++ src/cospaces/adapters/github_cli.py
@@ -10,3 +10,8 @@ class GitHubCommand:
     @property
     def argv(self) -> tuple[str, ...]:
         return ("gh", *self.arguments)
+
+
+class GitHubCliAdapter:
+    def prepare(self, arguments: tuple[str, ...]) -> GitHubCommand:
+        return GitHubCommand(arguments=arguments)
